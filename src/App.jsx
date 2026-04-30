be173a1dadbe37a550efbac29539587d50608e4c
ThanhsonWeb <sondeptroi@gmail.com>
2026-04-30 14:08:23 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<>
+			<h1>Hello Weather App here</h1>
+
+			<Header />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+   return (
+      <div>
+         <img src="" alt="" />
+         <span>Weather Now</span>
+      </div>
+   )
+}
+
+export default Header
